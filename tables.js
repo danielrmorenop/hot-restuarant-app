b1ec046daa6922b59b313edb91c5957f1672d7c6
torn75 <torn_75@yahoo.com>
2019-03-06 20:51:56 -0500

    added data

--- tables.js
+++ tables.js
@@ -1,14 +1,30 @@
 var currentRes = [
     {
-      name: "",
-      email: "",
-      phone: "",      
-    }
+      name: "alex torres",
+      email: "test@email.com",
+      phone: "3051235577",      
+    },
+    {
+        name: "brenda garcia",
+        email: "test2@email.com",
+        phone: "3051245577",      
+      },
+      {
+        name: "arthur arquist",
+        email: "test3@email.com",
+        phone: "3051255577",      
+      }  
 ];
 var waitList = [
     {
-        name: "",
-        email: "",
-        phone: "",  
+        name: "marisa timko",
+        email: "test4@gmail.com",
+        phone: "3057265353",  
+    },
+    {
+        name: "daniel moreno",
+        email: "test5@email.com",
+        phone: "3057467364",      
     }
 ];
+
